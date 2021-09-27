169adeaf6d19aafb4cde20673faf15ecd4cc0352
Apdo3939 <apdo3939@hotmail.com>
2021-09-27 13:19:06 -0300

    Backend Created

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+Sequencia para iniciar o projeto:
+Criar o arquivo package
+
+### yarn init
+
+Gerenciar as requesições, rotas e urls, enter outras
+
+### yarn add express
---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.listen(8081, () => {
+    console.log('Servidor rodando na porta 8081, http://localhost:8081')
+});
