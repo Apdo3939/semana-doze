ff67a91423aa95c422003cef1446bf505ee76f80
Apdo3939 <apdo3939@hotmail.com>
2021-10-05 16:40:43 -0300

    Add layout mobile part 02

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,33 @@ app.get('/list-messages/:sala', async (req, res) => {
 
 });
 
+app.get('/list-messages-mob/:sala', async (req, res) => {
+    const { sala } = req.params;
+    await Message.findAll({
+        order: [['id', 'DESC']],
+        where: { salaId: sala },
+        include: [{
+            model: User
+        },
+        {
+            model: Sala
+        }],
+    })
+        .then((data) => {
+            return res.json({
+                err: false,
+                data
+            });
+        })
+        .catch(() => {
+            return res.status(400).json({
+                err: true,
+                message: 'Nenhuma mensagem(s) encontrada(s)!',
+            });
+        })
+
+});
+
 app.get('/list-salas', async (req, res) => {
     await Sala.findAll({
         order: [['nome', 'ASC']],
@@ -160,7 +187,6 @@ const server = app.listen(8081, () => {
 io = socket(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
     socket.on("sala_conectar", (data) => {
         console.log("Sala selecionada " + data);
         socket.join(Number(data));
--- mobile/src/Chat.js
+++ mobile/src/Chat.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, TextInput, StyleSheet, View, TouchableOpacity, SafeAreaView, ScrollView } from "react-native";
+import { Text, TextInput, StyleSheet, View, TouchableOpacity, SafeAreaView, ScrollView, FlatList } from "react-native";
 import socketIOClient from "socket.io-client";
 import api from './configApi';
 import { RadioButton } from 'react-native-paper';
@@ -64,7 +64,7 @@ function Chat() {
             }
         }
         await socket.emit("enviar_mensagem", conteudoMensagem);
-        setListarMensagem([...listarMensagem, conteudoMensagem.conteudo]);
+        setListarMensagem([conteudoMensagem.conteudo, ...listarMensagem]);
         listarMensagens();
         setMensagem("");
     }
@@ -91,7 +91,7 @@ function Chat() {
     }
 
     const listarMensagens = async () => {
-        await api.get('/list-messages/' + sala)
+        await api.get('/list-messages-mob/' + Number(sala))
             .then((response) => {
                 setListarMensagem(response.data.data);
             })
@@ -118,75 +118,91 @@ function Chat() {
 
     useEffect(() => {
         socket.on("receber_mensagem", (data) => {
-            setListarMensagem([...listarMensagem, data]);
+            setListarMensagem([data, ...listarMensagem]);
             listarMensagens();
         });
     });
 
     return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <>
             {!logado ?
                 <SafeAreaView style={styles.container}>
-                    <Text style={styles.title}>Chat</Text>
-                    {status.type === 'erro' ? <Text>Usuario não encontrado</Text> : <Text></Text>}
-                    <Text style={styles.label}>Email</Text>
-                    <TextInput
-                        style={styles.input}
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                        autoCompleteType="off"
-                        placeholder="Email"
-                        value={email}
-                        onChangeText={(texto) => { setEmail(texto) }}
-                    />
-                    <Text style={styles.label}>Sala</Text>
-                    {salas.map((detSala) => {
-                        return (
-                            <View key={detSala.id} style={styles.radio}>
-                                <RadioButton
-                                    value={detSala.id}
-                                    status={sala === detSala.id ? 'checked' : 'unchecked'}
-                                    onPress={() => setSala(detSala.id)}
-                                />
-                                <Text style={styles.radioTexto}>
-                                    {detSala.nome}
-                                </Text>
-                            </View>
-                        )
-                    })}
-                    <TouchableOpacity
-                        style={styles.btnConectar}
-                        onPress={conectarSala}
-                    ><Text style={styles.btnConectarTitulo}>Conectar</Text></TouchableOpacity>
+                    <ScrollView>
+                        <Text style={styles.title}>Chat</Text>
+                        {status.type === 'erro' ? <Text>Usuario não encontrado</Text> : <Text></Text>}
+                        <Text style={styles.label}>Email</Text>
+                        <TextInput
+                            style={styles.input}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            autoCompleteType="off"
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={(texto) => { setEmail(texto) }}
+                        />
+                        <Text style={styles.label}>Sala</Text>
+                        {salas.map((detSala) => {
+                            return (
+                                <View key={detSala.id} style={styles.radio}>
+                                    <RadioButton
+                                        value={detSala.id}
+                                        status={sala === detSala.id ? 'checked' : 'unchecked'}
+                                        onPress={() => setSala(detSala.id)}
+                                    />
+                                    <Text style={styles.radioTexto}>
+                                        {detSala.nome}
+                                    </Text>
+                                </View>
+                            )
+                        })}
+                        <TouchableOpacity
+                            style={styles.btnConectar}
+                            onPress={conectarSala}
+                        ><Text style={styles.btnConectarTitulo}>Conectar</Text></TouchableOpacity>
+                    </ScrollView>
                 </SafeAreaView>
                 :
                 <SafeAreaView style={styles.container}>
                     <View>
                         <Text style={styles.title}>Nome: {nome}  Sala: {sala}</Text>
                     </View>
-                    <Text style={styles.subTitle}>Mensagem</Text>
-                    {listarMensagem.map((msg, key) => {
-                        return (
-                            <View key={key}>
-                                <Text>{msg.user.nome}: {msg.message}</Text>
-                            </View>
-                        )
-                    })}
-                    <TextInput
-                        style={styles.input}
-                        placeholder="mensagem..."
-                        value={mensagem}
-                        onChangeText={(texto) => { setMensagem(texto) }}
+
+                    <FlatList
+                        data={listarMensagem}
+                        renderItem={({ item }) => (
+                            <>
+                                {item.user.id === idUser
+                                    ?
+                                    <View style={styles.containerChatSender}>
+                                        <Text style={styles.containerChatTextSender}>{item.user.nome}: {item.message}</Text>
+                                    </View>
+                                    :
+                                    <View style={styles.containerChatReceiver}>
+                                        <Text style={styles.containerChatTextReceiver}>{item.user.nome}: {item.message}</Text>
+                                    </View>
+                                }
+                            </>
+                        )}
+                        keyExtractor={(item, index) => index.toString()}
                     />
-                    <TouchableOpacity
-                        onPress={enviarMensagem}
-                        style={styles.btnConectar}
-                    >
-                        <Text style={styles.btnConectarTitulo}>Enviar</Text>
-                    </TouchableOpacity>
+
+                    <View style={styles.formContent}>
+                        <TextInput
+                            style={styles.inputMessage}
+                            placeholder="mensagem..."
+                            value={mensagem}
+                            onChangeText={(texto) => { setMensagem(texto) }}
+                        />
+                        <TouchableOpacity
+                            onPress={enviarMensagem}
+                            style={styles.btnEnviar}
+                        >
+                            <Text style={styles.btnConectarTitulo}>Enviar</Text>
+                        </TouchableOpacity>
+                    </View>
                 </SafeAreaView>
             }
-        </ScrollView>
+        </>
     )
 }
 
@@ -257,9 +273,66 @@ export const styles = StyleSheet.create({
     },
     btnConectarTitulo: {
         textAlign: "center",
-        color: "#000099",
+        color: "#009900",
         fontSize: 24,
         fontWeight: "700"
+    },
+    btnEnviar: {
+        height: 40,
+        backgroundColor: "#e1e1e1",
+        borderWidth: 1,
+        borderColor: "#009900",
+        borderRadius: 8,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+    },
+
+    inputMessage: {
+        flex: 1,
+        height: 40,
+        borderWidth: 1,
+        padding: 10,
+        marginRight: 10,
+        borderRadius: 8,
+        borderColor: "#009900",
+        color: "#009900",
+        fontSize: 16
+    },
+
+    formContent: {
+        flexDirection: "row",
+        width: "100%",
+        marginVertical: 10,
+        height: 50,
+        alignItems: "center",
+    },
+    containerChatSender: {
+        width: "60%",
+        backgroundColor: "#00990080",
+        borderRadius: 8,
+        marginVertical: 4,
+        padding: 8,
+        alignSelf: "flex-end",
+    },
+    containerChatReceiver: {
+        width: "60%",
+        backgroundColor: "#00990020",
+        borderRadius: 8,
+        marginVertical: 4,
+        padding: 8,
+        alignSelf: "flex-start"
+    },
+    containerChatTextSender: {
+        color: "#e1e1e1",
+        fontSize: 18,
+        textAlign: "right"
+    },
+    containerChatTextReceiver: {
+        color: "#009900",
+        fontSize: 18,
+        textAlign: "left"
     },
 })
 
